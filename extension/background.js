c07294c74e8165de1d7027f58ad46e5c6febb04b
jbsiraudin <jb.siraudin@gmail.com>
2019-07-03 00:01:18 +0200

    Fix usage for one popup at a time which closes in amazon is closed.

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,56 @@
+var winId = null;
+var tabsId = [];
+
 chrome.runtime.onMessage.addListener(function(message) {
-  if (message.type && message.type == "buy_mode") {
-    chrome.windows.create({
-      url:
-        "https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU",
-      focused: false,
-      width: 200,
-      height: 300,
-      incognito: true
-    });
+  if (winId == null && message.type && message.type == "buy_mode") {
+    chrome.windows.create(
+      {
+        url:
+          "https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU",
+        focused: false,
+        width: 200,
+        height: 300,
+        incognito: false
+      },
+      function(win) {
+        winId = win.id;
+      }
+    );
+  }
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  if (tab.url.includes(".amazon.")) {
+    tabsId.push(tab.id);
+  }
+});
+
+chrome.tabs.onUpdated.addListener(function(id, { url }, tab) {
+  if (tab.url.includes(".amazon.") && tabsId && !tabsId.includes(tab.id)) {
+    tabsId.push(tab.id);
+  } else if (
+    !tab.url.includes(".amazon.") &&
+    tabsId &&
+    tabsId.includes(tab.id)
+  ) {
+    var index = tabsId.indexOf(tab.id);
+    tabsId.splice(index, 1);
+
+    if (tabsId.length == 0) {
+      chrome.windows.remove(winId);
+      winId = null;
+    }
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tab, info) {
+  if (tabsId.includes(tab)) {
+    var index = tabsId.indexOf(tab);
+    tabsId.splice(index, 1);
+
+    if (tabsId.length == 0) {
+      chrome.windows.remove(winId);
+      winId = null;
+    }
   }
 });
