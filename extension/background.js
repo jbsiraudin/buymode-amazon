fca9cbbe1f7a79b54f1c75e0c6ecd26da269274a
Charles Francoise <charles.francoise@gmail.com>
2019-07-04 14:00:16 +0200

    Start music based on choice

--- extension/background.js
+++ extension/background.js
@@ -1,31 +1,50 @@
 var winId = null;
 var tabsId = [];
 
-chrome.runtime.onMessage.addListener(function(message) {
-  if (winId == null && message.type && message.type == "buy_mode") {
-    chrome.windows.create(
-      {
-        url:
-          "https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU",
-        focused: false,
-        width: 200,
-        height: 300,
-        incognito: false
-      },
-      function(win) {
-        winId = win.id;
+const youtubeURLs = {
+  'buy-mode': 'https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU',
+  'wii-store': 'https://youtu.be/o0HIn9ksF4M'
+}
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (winId === null && message.type && message.type === "buy_mode") {
+    chrome.storage.sync.get(
+      { selectedTrack: 'buy-mode' },
+      ({ selectedTrack }) => {
+        let urls = [];
+        if (selectedTrack === 'both') {
+          urls.push(...Object.values(youtubeURLs));
+        } else if (selectedTrack === 'random') {
+          urls.push(Object.values(youtubeURLs)[Math.floor(Math.random() * 2)]);
+        } else {
+          urls.push(youtubeURLs[selectedTrack]);
+        }
+        for (const url of urls) {
+          chrome.windows.create(
+            {
+              url,
+              focused: false,
+              width: 200,
+              height: 300,
+              incognito: false
+            },
+            function (win) {
+              winId = win.id;
+            }
+          );
+        }
       }
     );
   }
 });
 
-chrome.tabs.onCreated.addListener(function(tab) {
+chrome.tabs.onCreated.addListener(function (tab) {
   if (tab.url.includes(".amazon.")) {
     tabsId.push(tab.id);
   }
 });
 
-chrome.tabs.onUpdated.addListener(function(id, { url }, tab) {
+chrome.tabs.onUpdated.addListener(function (id, { url }, tab) {
   if (tab.url.includes(".amazon.") && tabsId && !tabsId.includes(tab.id)) {
     tabsId.push(tab.id);
   } else if (
@@ -36,19 +55,19 @@ chrome.tabs.onUpdated.addListener(function(id, { url }, tab) {
     var index = tabsId.indexOf(tab.id);
     tabsId.splice(index, 1);
 
-    if (tabsId.length == 0) {
+    if (tabsId.length === 0) {
       chrome.windows.remove(winId);
       winId = null;
     }
   }
 });
 
-chrome.tabs.onRemoved.addListener(function(tab, info) {
+chrome.tabs.onRemoved.addListener(function (tab, info) {
   if (tabsId.includes(tab)) {
     var index = tabsId.indexOf(tab);
     tabsId.splice(index, 1);
 
-    if (tabsId.length == 0) {
+    if (tabsId.length === 0) {
       chrome.windows.remove(winId);
       winId = null;
     }
