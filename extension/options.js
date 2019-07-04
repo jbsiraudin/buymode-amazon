b42f2eafec078567f6dc4d57061c9366b7fed583
Charles Francoise <charles.francoise@gmail.com>
2019-07-04 13:46:06 +0200

    Add options panel

---
+++ extension/options.js
@@ -0,0 +1,23 @@
+
+function setSelectedTrack(selectedTrack) {
+  console.log('setting selected track', selectedTrack)
+  chrome.storage.sync.set({ selectedTrack });
+}
+
+function restoreOptions() {
+  chrome.storage.sync.get(
+    { selectedTrack: 'buy-mode' },
+    ({ selectedTrack }) => {
+      const radioButton = document.getElementById(selectedTrack);
+      radioButton.checked = true;
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selectedTrackRadioButtons = document.querySelectorAll('input[type="radio"][name="selected-track"]');
+  for (const r of selectedTrackRadioButtons) {
+    r.addEventListener('input', e => setSelectedTrack(e.target.value));
+  }
+  restoreOptions();
+});
