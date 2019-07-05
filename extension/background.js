d97d0f3c01398a86a1db6be8a983f77f5065e67d
Leon <leonekmi@hotmail.com>
2019-07-05 21:05:44 +0200

    Use local files instead of Youtube URLs. We do that to save bandwith. Use tab listeners instead of content scripts. We do that to speed up the music start.

--- extension/background.js
+++ extension/background.js
@@ -1,65 +1,70 @@
-var winId = null;
 var tabsId = [];
+var isPlaying = false;
 
-const youtubeURLs = {
-  "buy-mode":
-    "https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU",
-  "wii-store": "https://youtu.be/o0HIn9ksF4M",
-  "nook-store": "https://youtu.be/QAm7vgyOnN4"
+const musicObjs = {
+  "buy-mode": new Audio(chrome.runtime.getURL('sims.mp3')),//"https://youtu.be/399YneFTwh0?list=PL6CUjwslQrfi_Xwp4fvqBIMTmeofJq7xU",
+  "wii-store": new Audio(chrome.runtime.getURL('wiishop.mp3')),// "https://youtu.be/o0HIn9ksF4M",
+  "nook-store": new Audio(chrome.runtime.getURL('nook.mp3')),//"https://youtu.be/QAm7vgyOnN4"
 };
 
-chrome.runtime.onMessage.addListener(function(message) {
-  if (winId === null && message.type && message.type === "buy_mode") {
-    chrome.storage.sync.get(
-      { selectedTrack: "buy-mode" },
-      ({ selectedTrack }) => {
-        let urls = [];
-        if (selectedTrack === "both") {
-          urls.push(...Object.values(youtubeURLs));
-        } else if (selectedTrack === "random") {
-          urls.push(Object.values(youtubeURLs)[Math.floor(Math.random() * 2)]);
-        } else {
-          urls.push(youtubeURLs[selectedTrack]);
-        }
-        for (const url of urls) {
-          chrome.windows.create(
-            {
-              url,
-              focused: false,
-              width: 200,
-              height: 300,
-              incognito: false
-            },
-            function(win) {
-              winId = win.id;
-            }
-          );
-        }
+function stop() {
+  Object.keys(musicObjs).map(function (objKey, _index) {
+    musicObjs[objKey].pause();
+    musicObjs[objKey].currentTime = 0;
+  });
+  isPlaying = false;
+}
+
+function play() {
+  chrome.storage.sync.get(
+    { selectedTrack: "buy-mode" },
+    ({ selectedTrack }) => {
+      let music = [];
+      if (selectedTrack === "both") {
+        music.push(...Object.values(musicObjs));
+      } else if (selectedTrack === "random") {
+        music.push(Object.values(musicObjs)[Math.floor(Math.random() * 2)]);
+      } else {
+        music.push(musicObjs[selectedTrack]);
       }
-    );
-  }
-});
+      for (const currMusic of music) {
+        currMusic.play();
+        currMusic.loop = true;
+      }
+    }
+  );
+  isPlaying = true;
+}
 
-chrome.tabs.onCreated.addListener(function(tab) {
-  if (tab.url.includes(".amazon.")) {
-    tabsId.push(tab.id);
-  }
-});
+// chrome.runtime.onMessage.addListener(function(message, sender) {
+//   if (!isPlaying && message.type && message.type === "buy_mode" && sender.tab) {
+//     if (tabsId.find(el => el == sender.tab.id) === sender.tab.id) {
+//       return false;
+//     }
+//     play();
+//     tabsId.push(sender.tab.id);
+//     return true;
+//   }
+// });
 
-chrome.tabs.onUpdated.addListener(function(id, { url }, tab) {
-  if (tab.url.includes(".amazon.") && tabsId && !tabsId.includes(tab.id)) {
-    tabsId.push(tab.id);
-  } else if (
-    !tab.url.includes(".amazon.") &&
-    tabsId &&
-    tabsId.includes(tab.id)
-  ) {
-    var index = tabsId.indexOf(tab.id);
-    tabsId.splice(index, 1);
+chrome.tabs.onUpdated.addListener(function(id, _changes, tab) {
+  if (typeof tab.url == 'string') { // If we can access the url property, it means that's the URL is amazon (because the ext doesn't have access to other websites)
+    if (tabsId.includes(tab.id)) {
+      return;
+    } else {
+      tabsId.push(tab.id);
+      if (!isPlaying) { // This listener is faster than the content script, we use this to optimize script execution
+        play();
+      }
+    }
+  } else { // Else it means it's an other website than amazon
+    if (tabsId.includes(tab.id)) {
+      var index = tabsId.indexOf(tab);
+      tabsId.splice(index, 1);
 
-    if (tabsId.length === 0) {
-      chrome.windows.remove(winId);
-      winId = null;
+      if (tabsId.length === 0) {
+        stop();
+      }
     }
   }
 });
@@ -70,8 +75,7 @@ chrome.tabs.onRemoved.addListener(function(tab, info) {
     tabsId.splice(index, 1);
 
     if (tabsId.length === 0) {
-      chrome.windows.remove(winId);
-      winId = null;
+      stop();
     }
   }
 });
